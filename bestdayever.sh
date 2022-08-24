6a65eb63d9635e196c176411e20d0399b75650ae
Felbrou <15351171+Felbrou@users.noreply.github.com>
2022-08-24 10:22:10 -0300

    more studies

---
+++ bestdayever.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear;
+
+
+name="Evil Ben"
+
+
+echo "Good morning $name!!!";
+
+sleep 1
+
+echo "Olá $name!";
+exit; 
---
+++ userhello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "What is your name?";
+
+read name
+
+echo "Good morning $name";
+sleep 1
+echo "You're looking good today $name";
+sleep 1
+echo "You will have the best day you ever seen,$name!!!";
+
+exit;
+
